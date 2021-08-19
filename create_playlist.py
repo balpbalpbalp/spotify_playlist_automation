81a5b7d673949a20b9734e598f5f23bbea6e5c19
balpbalpbalp <narinoglubalp@gmail.com>
2021-08-19 23:19:53 +0300

    First Commit

---
+++ create_playlist.py
@@ -0,0 +1,27 @@
+# Which steps do I need to program to reach my goal?
+
+# 1. Log into YouTube.
+# 2. Go to my "Liked videos" page.
+# 3. Create a new playlist.
+# 4. Search for the liked song.
+# 5. Add it to the playlist.
+
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+    
+    def get_youtube_client(self): # 1. Log into YouTube.
+        pass
+
+    def get_liked_videos(self): # 2. Go to my "Liked videos" page.
+        pass
+
+    def create_spotify_playlist(self): # 3. Create a new playlist.
+        pass
+
+    def get_spotify_url(self): # 4. Search for the liked song.
+        pass
+
+    def add_song_to_playlist(self): # 5. Add it to the playlist.
+        pass
